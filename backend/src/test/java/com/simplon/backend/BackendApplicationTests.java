9f53f5da8ecd5adf943d37e3129b95ef9e09da89
unknown <emma.broudic35@gmail.com>
2024-03-05 10:01:13 +0100

    modèle de données

--- backend/src/test/java/com/simplon/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/simplon/backend/BackendApplicationTests.java
@@ -1,10 +1,10 @@
-package com.simplon.back;
+package com.simplon.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BackApplicationTests {
+class BackendApplicationTests {
 
 	@Test
 	void contextLoads() {
