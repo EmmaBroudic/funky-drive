14fa6213da4b946284f6f24d6d9cfd6ed5ee7d8f
unknown <emma.broudic35@gmail.com>
2024-03-13 09:18:42 +0100

    redirection après connexion ok

--- frontend/src/pages/signup/signup.jsx
+++ frontend/src/pages/signup/signup.jsx
@@ -1,24 +1,56 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 import './signup.css';
 
 function Signup() {
-    return (
-        <div className="signup-body">
-            <h2>Sign up</h2>
-            <form action="/submit" method="post">
-                <div>
-                    <label htmlFor="email">Email:</label>
-                    <input type="email" id="email" name="email" required />
-                </div>
-                <div>
-                    <label htmlFor="password">Password:</label>
-                    <input type="password" id="password" name="password" required />
-                </div>
-                <div>
-                    <button type="submit">Submit</button>
-                </div>
-            </form>
+  const [users, setUsers] = useState([]);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    axios.get("http://localhost:8080/user/all")
+      .then(response => {
+        setUsers(response.data);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }, []);
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    const userEmail = event.target.email.value;
+    const userPassword = event.target.password.value;
+
+    const user = users.find(u => u.email === userEmail && u.password === userPassword);
+
+    if (user) {
+      console.log("Connection réussie");
+      // Utilise history.push pour rediriger l'utilisateur
+      navigate('/home');
+    } else {
+      console.log("Mauvais mot de passe");
+    }
+  };
+
+  return (
+    <div className="signup-body">
+      <h2>Sign up</h2>
+      <form onSubmit={handleFormSubmit}>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input type="email" id="email" name="email" required />
         </div>
-    );
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input type="password" id="password" name="password" required />
+        </div>
+        <div>
+          <button type="submit">Submit</button>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default Signup;
